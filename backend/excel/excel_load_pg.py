90f1fb74affc59a1780604b949a9962e30a78818
NzKoff <nskvip54@gmail.com>
2020-10-24 17:45:57 +0700

    Переработал код, теперь может Собирать из таблиц ЛЭП, если есть есть необходимые заголовки по Аналогии с таблицами П_(ЛЭП)_2019 и П_Н_Хар-ка действующих...

--- backend/excel/excel_load_pg.py
+++ backend/excel/excel_load_pg.py
@@ -2,89 +2,132 @@ from base.base_sql import Sql
 import excel.excel_parse as ep
 from excel.excel_base import *
 from excel.excel_sql import *
-import json
 
 
-#result = Sql.exec(query=sql)
-
-def load_pk_char(file):
-    table1, table2 = ep.parse_pk_char(file)
-
-
-def load_pn_char(file):
-    table = ep.parse_pn_char(file)
+def load_lep_table(answers):
+    col_num_pp = None
+    col_name_lep = None
+    col_god_vvoda = None
+    col_disp_name = None
+    col_napr = None
+    col_colcep = None
+    col_dlpotr = None
+    col_dluchpotr = None
+    col_dlpocep = None
+    col_dluchpocep = None
+    col_marka = None
+    col_tehsost = None
+    for k in range(20):
+        for i in range(len(answers[k])):
+            if str(answers[k][i]).lower() in ["№ п/п", "№", 'п/п']:
+                col_num_pp = i
+            elif "наименование лэп" in str(answers[k][i]).lower():
+                col_name_lep = i
+            elif "диспетчерский номер" in str(answers[k][i]).lower():
+                col_disp_name = i
+            elif "год ввода" in str(answers[k][i]).lower():
+                col_god_vvoda = i
+            elif "U ном".lower() in str(answers[k][i]).lower() or "напряжение" in str(answers[k][i]).lower():
+                col_napr = i
+            elif "цепей" in str(answers[k][i]).lower():
+                col_colcep = i
+            elif "по трассе" in str(answers[k][i]).lower() and (
+                    "всего" in str(answers[k - 2][i]).lower() or "провод" in str(answers[k - 1][i]).lower()):
+                col_dlpotr = i
+            elif "по трассе" in str(answers[k][i]).lower() and "в том числе" in str(answers[k - 2][i]).lower():
+                col_dluchpotr = i
+            elif "по цепям" in str(answers[k][i]).lower() and "всего" in str(answers[k - 2][i-1]).lower():
+                col_dlpocep = i
+            elif "по цепям" in str(answers[k][i]).lower() and "в том числе" in str(answers[k - 2][i-1]).lower():
+                col_dluchpocep = i
+            elif "марка" in str(answers[k][i]).lower() and (
+                        "провод" in str(answers[k - 1][i]).lower() or "провод" in str(answers[k - 1][i - 1]).lower()):
+                col_marka = i
+            elif "техническое состояние" in str(answers[k][i]).lower() or "заключение, при-нятое по рез-там то" in str(
+                answers[k][i]).lower():
+                col_tehsost = i
     num_set = None
     num_pp = None
     id_lep = None
     year = None
     colcep = None
     techsost = None
-    for i in range(len(table)):
-        if 'Сети' in str(table[i][0]):
-            num_set = table[i][0][5:]
+    dlport = None
+    dlpocep = None
+    dluchpotr = None
+    dluchpocep = None
+    disp_name = None
+    napr = None
+    for i in range(len(answers)):
+        if i < 20:
+            if 'Сети' not in str(answers[i]) and '-' not in str(answers[i][col_marka]):
+                continue
+        if 'Сети' in str(answers[i][0]):
+            num_set = answers[i][0][5:]
             continue
-        if table[i][0] is not None:
-            num_pp = table[i][0]
-            year = None
-            colcep = None
-            techsost = None
-        if table[i][1] is not None:
-            if table[i][25] is not None:
-                techsost = table[i][25]
-            query_json_format = {
-                "LEP": str(LEP).replace("'", '"'),
-                "num_set": num_set,
-                "num_pp": num_pp,
-                "name": table[i][1],
-                "disp_name": table[i][3],
-                "napr": table[i][2],
-                "techsost": techsost
-            }
-            query = SQL_ADD_LEP.format(**query_json_format).replace("'None'", "NULL").replace("None", "NULL")
-            id_lep = int(Sql.exec(query=query)[0]['id'])
-        if table[i][19] is not None:
-            year = table[i][19]
-        if table[i][4] is not None:
-            colcep = table[i][4]
+        if col_num_pp is not None:
+            if answers[i][col_num_pp] is not None:
+                num_pp = answers[i][col_num_pp]
+                year = None
+                colcep = None
+                techsost = None
+        if col_name_lep is not None:
+            if answers[i][col_name_lep] is not None:
+                if col_name_lep is not None:
+                    if answers[i][col_tehsost] is not None:
+                        techsost = answers[i][col_tehsost]
+                if col_napr is not None:
+                    napr = answers[i][col_napr]
+                if col_disp_name is not None:
+                    disp_name = answers[i][col_disp_name]
+                query_json_format = {
+                    "LEP": str(LEP).replace("'", '"'),
+                    "num_set": num_set,
+                    "num_pp": num_pp,
+                    "name": answers[i][col_name_lep],
+                    "disp_name": disp_name,
+                    "napr": napr,
+                    "techsost": techsost
+                }
+                query = SQL_ADD_LEP.format(**query_json_format).replace("'None'", "NULL").replace("None", "NULL")
+                id_lep = int(Sql.exec(query=query)[0]['id'])
+        if col_god_vvoda is not None:
+            if answers[i][col_god_vvoda] is not None:
+                year = answers[i][col_god_vvoda]
+        if col_colcep is not None:
+            if answers[i][col_colcep] is not None:
+                colcep = answers[i][col_colcep]
+        if col_dlpotr is not None:
+            dlport = answers[i][col_dlpotr]
+        if col_dlpocep is not None:
+            dlpocep = answers[i][col_dlpocep]
+        if col_dluchpotr is not None:
+            dluchpotr = answers[i][col_dluchpotr]
+        if col_dluchpocep is not None:
+            dluchpocep = answers[i][col_dluchpocep]
         query_json_format = {
             "PROVOD": str(PROVOD).replace("'", '"'),
             "id_lep": id_lep,
-            "name": table[i][11],
+            "name": answers[i][col_marka],
             "year": year,
             "colcep": colcep,
-            "dlpotr": table[i][5],
-            "dlpocep": table[i][6],
-            "dluchpotr": table[i][7],
-            "dluchpocep": table[i][8],
+            "dlpotr": dlport,
+            "dlpocep": dlpocep,
+            "dluchpotr": dluchpotr,
+            "dluchpocep": dluchpocep,
         }
         Sql.exec(query=SQL_ADD_PROVOD.format(**query_json_format).replace("'None'", "NULL").replace("None", "NULL"))
 
 
-def load_p_lep_with_question_list(file):
-    table = ep.parse_p_lep_with_question_list(file)
 
+def excel_load(file):
+    answers = ep.parse_all_table(file)
+    for answer in answers:
+        if "ЛЭП" in str(answer):
+            load_lep_table(answer)
 
-def load_pn_char_transf(file):
-    table = ep.parse_pn_char_transf(file)
-
-
-def load_p_sil_transf(file):
-    table = ep.parse_p_sil_transf(file)
-
-
-def excel_load(status, file):
-    if status == "1":
-        load_pk_char(file)
-    elif status == "2":
-        load_pn_char(file)
-    elif status == "3":
-        load_p_lep_with_question_list(file)
-    elif status == "4":
-        load_pn_char_transf(file)
-    elif status == "5":
-        load_p_sil_transf(file)
 
 # П_К_хар.ПС и ВЛ.xlsx
 
-excel_load('2', 'П_Н_Хар-ка действующих ВЛ и КЛ 110 кВ.xlsx')
+excel_load('П_(ЛЭП)_2019_к_опросному_листу.xlsx')
 
--- backend/excel/excel_parse.py
+++ backend/excel/excel_parse.py
@@ -2,53 +2,21 @@ import xlwings as xw
 import pandas as pd
 
 
-# П_К_хар.ПС и ВЛ.xlsx
-def parse_pk_char(file):
-    data = parse_all_table(file, 'Таблица 2.2', 'A7', 'M')
-    transform = []
-    komm_apparat = []
-    reaktor = []
-    for dat in data:
-        if dat[1] is not None:
-            transform.append(dat[0:24])
-        if dat[24] is not None:
-            komm_apparat.append(dat[0, 24:28])
-        if dat[28] is not None:
-            reaktor.append()
-    second_data = parse_all_table(file, 'Таблица 4', 'A8', 'AG')
-    return data, second_data
-
-
-# П_Н_Хар-ка действующих ВЛ и КЛ 110 кВ.xlsx
-def parse_pn_char(file):
-    data = parse_all_table(file, '8', 'A11', 'AA')
+def parse_all_table(file):
+    xl = pd.ExcelFile(file)
+    lists = xl.sheet_names
+    data = []
+    for i in lists:
+        answer = parse_table(file, i, 'A1', 'AZ')
+        if len([k for k in answer if k is not None]) > 0:
+            data.append(answer)
     return data
 
 
-# П_(ЛЭП)_2019_к_опросному_листу.xlsx
-def parse_p_lep_with_question_list(file):
-    data = parse_all_table(file, 'Лист1', 'A9', 'M')
-    return data
-
-
-# П_Н_Хар-ка тр-ров 110 кВ.xlsx
-def parse_pn_char_transf(file):
-    data = parse_all_table(file, 'Лист1', 'A6', 'AY')
-    return data
-
-
-# П_силовые тр-ры.xlsx
-def parse_p_sil_transf(file):
-    data = parse_all_table(file, 'Лист1', 'A6', 'As')
-    return data
-
-
-def parse_all_table(file, sheet_name, range_first, range_last):
+def parse_table(file, sheet_name, range_first, range_last):
     wb = xw.Book(file)
     last_row = len(pd.read_excel(file, sheet_name=sheet_name)) + 1
     data_excel = wb.sheets[sheet_name]
     data_pd = data_excel.range('{first}:{last}{row}'.format(first=range_first, last=range_last, row=last_row)).value
     return data_pd
 
-#ans = parse_p_sil_transf('П_силовые тр-ры.xlsx')
-#print(ans)
